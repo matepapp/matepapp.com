19e9743c9416c93362164301b2e5d3a5c6754171
Mate Papp <matepapp@icloud.com>
2020-03-11 11:17:40 +0100

    Update data and add profile picture

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const FeatherIconLink: FC<{ href: string }> = ({ children, href }) => (
   <a
     href={href}
-    className="mx-1 duration-300 ease-in-out transform hover:scale-105 hover:text-yellow-300"
+    className="mx-2 duration-300 ease-in-out transform hover:scale-105 hover:text-yellow-300"
   >
     <svg
       viewBox="0 0 24 24"
@@ -17,28 +17,49 @@ const FeatherIconLink: FC<{ href: string }> = ({ children, href }) => (
 );
 
 const IndexPage = () => (
-  <div className="flex flex-col items-center justify-center">
-    <h1 className="text-5xl font-bold">Mate Papp</h1>
-    <h2 className="text-xl text-gray-300">Software Engineer</h2>
-    <div className="text-gray-200 text-md">
-      Co-Founder and CTO{" "}
-      <a
-        href="https://cogito.study"
-        className="transition duration-300 ease-in-out hover:text-yellow-300"
-      >
-        @cogito.study
-      </a>
+  <div className="flex flex-row items-center justify-center max-w-4xl">
+    <div className="flex flex-col w-1/2 p-2">
+      <h1 className="text-5xl font-bold">Mate Papp</h1>
+      <div className="text-xl text-gray-300">
+        Co-Founder and CTO{" "}
+        <a
+          href="https://cogito.study"
+          className="text-yellow-300 transition duration-300 ease-in-out hover:underline"
+        >
+          @cogito.study
+        </a>
+      </div>
+      <p className="mt-4 leading-relaxed">
+        Crafting amazing digital experiences with GraphQL, React, TypeScript,
+        NextJS, Gatsby, Swift.
+      </p>
+      <div className="flex mt-6">
+        <FeatherIconLink href="https://twitter.com/matepappp">
+          <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+        </FeatherIconLink>
+        <FeatherIconLink href="https://instagram.com/pappmate">
+          <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
+          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+          <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
+        </FeatherIconLink>
+        <FeatherIconLink href="https://facebook.com/pappmatesz">
+          <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+        </FeatherIconLink>
+        <FeatherIconLink href="https://github.com/matepapp">
+          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+        </FeatherIconLink>
+        <FeatherIconLink href="https://www.linkedin.com/in/matepapp/">
+          <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+          <rect x="2" y="9" width="4" height="12" />
+          <circle cx="4" cy="4" r="2" />
+        </FeatherIconLink>
+        <FeatherIconLink href="mailto:matepapp@icloud.com">
+          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+          <polyline points="22,6 12,13 2,6" />
+        </FeatherIconLink>
+      </div>
     </div>
-    <div className="flex mt-4">
-      <FeatherIconLink href="https://instagram.com/pappmate">
-        <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
-        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-      </FeatherIconLink>
-      <FeatherIconLink href="https://facebook.com/pappmatesz">
-        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-      </FeatherIconLink>
-    </div>
+    <img src="/assets/profile.jpeg" alt="profile" className="w-1/2 shadow-xl" />
   </div>
 );
 
