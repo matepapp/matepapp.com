b4863a152be3a55d4d6031410fa83d5062dcc47c
Mate Papp <matepapp@icloud.com>
2020-04-02 12:41:54 +0200

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,13 @@ import "../css/tailwind.css";
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <link
         href="https://fonts.googleapis.com/css2?family=Rubik:wght@500;700&display=swap"
         rel="stylesheet"
       />
+      <link rel="icon" href="/assets/favicon.ico" />
     </Head>
     <Layout>
       <Component {...pageProps} />
