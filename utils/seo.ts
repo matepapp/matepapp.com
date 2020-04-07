13e9d15167f3fcb01162d8f6784afe5695f4f42a
Mate Papp <matepapp@icloud.com>
2020-04-07 20:30:52 +0200

    Fix unused a letter

--- utils/seo.ts
+++ utils/seo.ts
@@ -1,6 +1,6 @@
 export const SEO = {
   title: "Mate Papp",
   titleTemplate: (title: string) => `${title} | Mate Papp`,
-  description: `My name is Mate Papp, a 24-year-old a Software Engineer, Designer, Entrepreneur, currently based in Budapest, Hungary. I'm a lifelong learner & teacher, an Apple fanboy, a GraphQL enthusiast and a really fast speaker.`,
+  description: `My name is Mate Papp, a 24-year-old Software Engineer, Designer, Entrepreneur, currently based in Budapest, Hungary. I'm a lifelong learner & teacher, an Apple fanboy, a GraphQL enthusiast and a really fast speaker.`,
   url: (path: string) => `https://matepapp.com${path}`,
 };
