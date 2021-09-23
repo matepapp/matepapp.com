bffc6d1fd0648e4146f59d4a25ab620cc725ea10
Mate Papp <matepapp@icloud.com>
2021-09-23 15:33:33 +0200

    Remove theme color meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -70,10 +70,6 @@ class MyDocument extends Document {
           />
           <meta name="msapplication-TileColor" content="#34D399" />
           <meta name="msapplication-TileImage" content="/assets/favicon/ms-icon-144x144.png" />
-          {/* @ts-ignore */}
-          <meta name="theme-color" content="#F4F4F5" media="(prefers-color-scheme: light)" />
-          {/* @ts-ignore */}
-          <meta name="theme-color" content="#18181B" media="(prefers-color-scheme: dark)" />
         </Head>
         <body className="text-primary dark:bg-gray-900 antialiased bg-gray-100">
           <Main />
