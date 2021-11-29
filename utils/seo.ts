0136d65a5c47b82a32ebca15e52d1c2a98a3e339
Mate Papp <matepapp@icloud.com>
2021-11-29 23:54:40 +0100

    update seo

--- utils/seo.ts
+++ utils/seo.ts
@@ -1,6 +1,6 @@
 export const SEO = {
   title: 'Mate Papp',
   titleTemplate: (title: string) => `${title} | Mate Papp`,
-  description: `I'm Mate Papp, a Software Engineer, a lifelong learner and teacher, an Apple fanboy, and a productivity nerd (= too lazy). Originally from Hungary, currently living in beautiful Barcelona, Spain with my lovely girlfriend.`,
+  description: `I'm Mate Papp, a Software Engineer, a lifelong learner and teacher, an Apple fanboy, a wannabe home barista, and a productivity nerd (= too lazy). Originally from Hungary, currently living in beautiful Barcelona, Spain with my beloved girlfriend.`,
   url: (path: string) => `https://matepapp.com${path}`,
 }
